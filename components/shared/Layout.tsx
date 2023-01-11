1714e9cd54ba31c89e1281704d19e1e4978fda55
unknown <fahimismail75.scc@gmail.com>
2023-01-11 22:08:39 +0600

    ts-daisy-next

---
+++ components/shared/Layout.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head"
+import Navbar from "./Header"
+import Footer from "./Footer"
+
+type Props = {
+    title: string;
+    children?: JSX.Element;
+    description?: string;
+};
+
+const Layout = ({ children , title, description }: Props) => {
+    return (
+        <div>
+
+            <Head>
+                <title>{title ? `${title}` : `ICT |BUP`}</title>
+                {description && <meta name="description" content={description}></meta>}
+                <link rel="icon" href="/img/bup.png" />
+            </Head>
+
+            <Navbar />
+
+            <main className="min-h-screen">
+                {children}
+            </main>
+
+            <Footer />
+
+
+        </div>
+    )
+}
+
+export default Layout
