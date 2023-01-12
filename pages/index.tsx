308977f81957ecc0d3e305633d63ddd628c7d95a
unknown <fahimismail75.scc@gmail.com>
2023-01-12 15:01:52 +0600

    z

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 import Layout from '../components/shared/Layout'
 import Home from '../components/page components/Home'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function index() {
+export default function index({data}: any) {
   return (
     <Layout title='Ts next Daisy 👌🤖'>
       <Home />
     </Layout>
   )
+}
+
+export async function getServerSideProps(context: any) {
+  return {
+    props: {
+      data: 'hello'
+    }, // will be passed to the page component as props
+  }
 }
