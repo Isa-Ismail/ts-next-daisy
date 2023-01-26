067efbe6d85cad84963d09f269305af840b68a8a
isa-ismail <fahimismail75.scc@gmail.com>
2023-01-26 13:41:16 +0600

    cloudinary

--- components/page components/Home.tsx
+++ components/page components/Home.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -43,13 +44,33 @@ const Home: React.FC<Props> = () => {
     })
   }
 
+  const [image, setImage] = useState('')
+  const [url, setUrl] = useState('')
+
+  const sub = (e: any) => {
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('file', image)
+    formData.append('upload_preset', 'fsns6mal')
+    fetch('https://api.cloudinary.com/v1_1/dfffbcsa8/image/upload', {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.json())
+      .then(data => setUrl(data.url))
+      .catch(err => console.log(err))
+    console.log(image)
+  }
+
   const data = useFetch('https://selise.vercel.app/api/products')
   console.log(data)
 
   return (<div>
                 <div className="hero min-h-screen bg-base-200">
                   <div className="hero-content text-center">
-                    <div className="max-w-md">
+                  <div className="max-w-md">
+                        <input type="file" onChange={(e: any) => setImage(e.target.files[0])}/>
+                        <button className='bg-red-300 p-2 rounded-lg' onClick={sub}>Submit</button>
                         <h1 className="text-5xl font-bold">Ts next Daisy { cart.cartItems&&cart.cartItems[0]&&cart.cartItems[0].id } 👌🤖</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <button onClick={() => dispatch(addCart(product, 10))} className="btn btn-outline btn-accent">Get Started</button>
