80209d9cd41e26cfc9b03fcc3e8dddad316091b9
isa-ismail <fahimismail75.scc@gmail.com>
2023-03-16 23:36:10 +0600

    ham-nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '376px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1250px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
