b46da8f727bff1e78b434a87377c13ef5c242cca
isa-ismail <fahimismail75.scc@gmail.com>
2023-03-27 21:40:54 +0600

    icon

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -19,24 +19,24 @@ const Header = () => {
 
   return (
   <nav className={`${show?"min-h-[16rem] bg-slate-500 shadow-md fixed z-10 w-screen":"bg-slate-500 shadow-md fixed z-10 w-screen"}`}>
-  <div className="lg:flex justify-between">
-    <ul className="flex space-x-4 p-4 sm:hidden lg:flex">
+  <div className="md:flex justify-between p-2">
+    <ul className="flex space-x-4 p-4 sm:hidden md:flex">
       <li className="cursor-pointer p-2 hover:bg-white">Home</li>
       <li className="cursor-pointer p-2 hover:bg-white">page 1</li>
       <li className="cursor-pointer p-2 hover:bg-white">page 2</li>
       <li className="cursor-pointer p-2 hover:bg-white">page 3</li>
     </ul>
-    {!show?<ul className="space-x-4 p-4 sm:block lg:hidden">
+    {!show?<ul className="space-x-4 p-4 sm:block md:hidden">
       <li className="cursor-pointer p-2" onClick={() => setShow (e=> !e)}>
         <Image alt="" src={'/favicon.ico'} width={30} height={30} />
       </li>
-    </ul>:<ul className="space-x-4 p-4 sm:block lg:hidden">
+    </ul>:<ul className="space-x-4 p-4 sm:block md:hidden">
       <li className="text-white cursor-pointer p-2" onClick={() => setShow (e=> !e)}>
-        X
+        <img alt="" src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Flat_cross_icon.svg/512px-Flat_cross_icon.svg.png?20170316053531' width={30} height={30} />
       </li>
     </ul>}
-    <ul className="space-x-4 p-4 sm:hidden lg:flex">
-      <li className="cursor-pointer p-2 hover:bg-white">a</li>
+    <ul className="space-x-4 p-4 sm:hidden md:flex">
+      <li className="cursor-pointer p-2 hover:bg-white">Login / Sign Up</li>
     </ul>
     {show?<ul className="flex flex-col justify-center z-20 w-screen items-center">
       <li className="cursor-pointer p-2 hover:bg-white">Home</li>
