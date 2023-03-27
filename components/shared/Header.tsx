12d24a503640be5c76875e17dd66b7149cd1246e
isa-ismail <fahimismail75.scc@gmail.com>
2023-03-27 21:53:30 +0600

    &&

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -18,34 +18,62 @@ const Header = () => {
   }, [show])
 
   return (
-  <nav className={`${show?"min-h-[16rem] bg-slate-500 shadow-md fixed z-10 w-screen":"bg-slate-500 shadow-md fixed z-10 w-screen"}`}>
-  <div className="md:flex justify-between p-2">
-    <ul className="flex space-x-4 p-4 sm:hidden md:flex">
-      <li className="cursor-pointer p-2 hover:bg-white">Home</li>
-      <li className="cursor-pointer p-2 hover:bg-white">page 1</li>
-      <li className="cursor-pointer p-2 hover:bg-white">page 2</li>
-      <li className="cursor-pointer p-2 hover:bg-white">page 3</li>
-    </ul>
-    {!show?<ul className="space-x-4 p-4 sm:block md:hidden">
-      <li className="cursor-pointer p-2" onClick={() => setShow (e=> !e)}>
-        <Image alt="" src={'/favicon.ico'} width={30} height={30} />
-      </li>
-    </ul>:<ul className="space-x-4 p-4 sm:block md:hidden">
-      <li className="text-white cursor-pointer p-2" onClick={() => setShow (e=> !e)}>
-        <img alt="" src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Flat_cross_icon.svg/512px-Flat_cross_icon.svg.png?20170316053531' width={30} height={30} />
-      </li>
-    </ul>}
-    <ul className="space-x-4 p-4 sm:hidden md:flex">
-      <li className="cursor-pointer p-2 hover:bg-white">Login / Sign Up</li>
-    </ul>
-    {show?<ul className="flex flex-col justify-center z-20 w-screen items-center">
-      <li className="cursor-pointer p-2 hover:bg-white">Home</li>
-      <li className="cursor-pointer p-2 hover:bg-white">page 2</li>
-      <li className="cursor-pointer p-2 hover:bg-white">page 3</li>
-      <li className="cursor-pointer p-2 hover:bg-white">page 4</li>
-    </ul>:null}
-  </div>
-</nav>)
+    <nav className={`${show ? "min-h-[16rem] bg-slate-500 shadow-md fixed z-10 w-screen" : "bg-slate-500 shadow-md fixed z-10 w-screen"}`}>
+      
+      <div className="md:flex justify-between p-2">
+        
+        <ul className="flex space-x-4 p-4 md:flex sm:hidden">
+          
+          <li className="cursor-pointer p-2 hover:bg-white text-lg">Home</li>
+          
+          <li className="cursor-pointer p-2 hover:bg-white">page 1</li>
+          
+          <li className="cursor-pointer p-2 hover:bg-white">page 2</li>
+          
+          <li className="cursor-pointer p-2 hover:bg-white">page 3</li>
+          
+        </ul>
+        
+        {!show ? <ul className="space-x-4 p-4 sm:block md:hidden">
+          
+          <li className="cursor-pointer p-2" onClick={() => setShow(e => !e)}>
+            
+            <Image alt="" src={'/favicon.ico'} width={30} height={30} />
+            
+          </li>
+          
+        </ul> : <ul className="space-x-4 p-4 sm:block md:hidden">
+            
+            <li className="text-white cursor-pointer p-2" onClick={() => setShow(e => !e)}>
+              
+              <img alt="" src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Flat_cross_icon.svg/512px-Flat_cross_icon.svg.png?20170316053531' width={30} height={30} />
+              
+            </li>
+            
+        </ul>}
+        
+        <ul className="space-x-4 p-4 sm:hidden md:block">
+          
+          <li className="cursor-pointer p-2 hover:bg-white">Login / Sign Up</li>
+          
+        </ul>
+        
+        {show && <ul className="flex flex-col justify-center z-20 w-screen items-center">
+          
+          <li className="cursor-pointer p-2 hover:bg-white">Home</li>
+          
+          <li className="cursor-pointer p-2 hover:bg-white">page 2</li>
+          
+          <li className="cursor-pointer p-2 hover:bg-white">page 3</li>
+          
+          <li className="cursor-pointer p-2 hover:bg-white">page 4</li>
+          
+        </ul> }
+        
+      </div>
+      
+    </nav>)
+  
 }
 
 export default Header
